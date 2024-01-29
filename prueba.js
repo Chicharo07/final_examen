34bb04650b1d2dab40be01f6114b9a5a0515e4c3
Chicharo07 <155588449+Chicharo07@users.noreply.github.com>
2024-01-28 20:26:55 -0600

    Add files via upload

--- prueba.js
+++ prueba.js
@@ -5,20 +5,36 @@ let pais = document.getElementById("pais");
 let continente = document.getElementById("continente");
 let zona_horaria = document.getElementById("zona_horaria");
 
-const solicitudAPI = async () => {
-  try {
-    const response = await axios.get("https://ipinfo.io/json");
-    console.log(response.data);
-    ip.innerHTML = response.data.ip;
-    pais.innerHTML = response.data.country;
-    continente.innerHTML = response.data.region;
-    zona_horaria.innerHTML = response.data.timezone;
-  } catch (error) {
-    console.log(error);
-  }
+let pais_detectado;
+
+const solicitudAPI = () => {
+  // Obtener el país detectado
+  pais_detectado = axios
+  .get("https://api.ipgeolocation.io/v1/?apiKey=YOUR_API_KEY&ip=YOUR_IP_ADDRESS")
+  .then(function (response) {
+    // Manejar respuesta exitosa
+    console.log(response.data);
+    pais.innerHTML = response.data.country;
+  })
+  .catch(function (error) {
+    // Manejar error
+    console.log(error);
+  });
 };
 
 //llama al evento LOAD cada vez que se actualiza la pagina 
 //y llama a la version solicitudAPI que tiene la rutina
 //de llamar a API desde Axios
 window.addEventListener('load',solicitudAPI );
+
+// Verificar el país detectado
+if (pais_detectado === "Sudamerica") {
+  // Redirigir a la API de sudamerica
+  window.location.href = "https://itp-scr-examen-1.000webhostapp.com/php-geoip-api/index.php";
+} else if (pais_detectado === "Africa") {
+  // Redirigir a la API de Africa
+  window.location.href = "https://itp-examen.000webhostapp.com/php-geoip-api/index.php";
+} else {
+  // Redirigir a la API de Europa
+  window.location.href = "https://itp-europa.000webhostapp.com/php-geoip-api/index.php";
+}
